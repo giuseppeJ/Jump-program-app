8fdcbe4b33f481903bf7b25d91579f61dfccb24e
giuseppeJ <joeyarmas@gmail.com>
2025-03-31 12:18:48 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Dunk Training App</h1>
+      <p>Week-by-week plan, calendar, and jump tracker coming soon!</p>
+    </div>
+  );
+}
